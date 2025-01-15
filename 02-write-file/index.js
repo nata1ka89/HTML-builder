211d7ca45d0e953cc4a38e96bbff32b4f13cb9da
nata1ka <nata1ka.yurevich@gmail.com>
2025-01-15 15:07:45 +0300

    feat: add task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+stdout.write('Hello! Please, write your text.\n');
+fs.writeFile(path.join(__dirname, 'text.txt'), '', (err) => {
+  if (err) throw err;
+});
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    stdout.write('Bye bye!');
+    process.exit();
+  } else {
+    fs.appendFile(path.join(__dirname, 'text.txt'), data, (err) => {
+      if (err) throw err;
+      stdout.write('Write more text.\n');
+    });
+  }
+});
+
+process.on('SIGINT', () => {
+  stdout.write('Bye bye!');
+  process.exit();
+});
